fdfe0091f181d302b8e8ea285d714bf0bbf53d2a
BORLET BENJAMIN p1502484 <benjamin.borlet@etu.univ-lyon1.fr>
2017-03-02 21:17:32 +0100

    Page de presentation et .htaccess

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -14,48 +14,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	body {
 		background-color: #fff;
 		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		font: 20px normal Helvetica, Arial, sans-serif;
 		color: #4F5155;
 	}
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
+	img {
+		float: right;
+		width: 50%;
 	}
 
 	h1 {
+		display: inline-block;
 		color: #444;
 		background-color: transparent;
 		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
+		font-size: 24px;
+		font-weight: 600;
 		margin: 0 0 14px 0;
 		padding: 14px 15px 10px 15px;
 	}
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
 	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
+		margin: 0 15px;
 	}
 
 	#container {
@@ -68,21 +48,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Work In Progress !</h1><img src="assets/images/teckmeb_logo.jpg" alt="Logo Teckmeb"/>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>Le site web que vous recherchez est en cours de développement !</p>
+		<p>Un prototype devrait être prêt en Juin,</p>
+		<p>Et le site sera en état de fonctionner fin 2017 (même avant, si les dev' sont motivés !)</p>
 	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
 </body>
