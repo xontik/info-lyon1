1dd311a93c6879013a00392a41b075a15b32b0d8
BORLET BENJAMIN p1502484 <benjamin.borlet@etu.univ-lyon1.fr>
2017-04-13 16:04:38 +0200

    Ajout model notes

---
+++ application/models/notes_model.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: xontik
+ * Date: 13/04/2017
+ * Time: 15:42
+ */
+class Notes_model extends CI_Model {
+
+    public function __construct()
+    {
+        
+    }
+
+}
