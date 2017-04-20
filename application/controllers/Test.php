afa6f7180ef45cef4c93f3fe09526d4078df27e9
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2017-04-20 16:02:02 +0200

    Pas besoin de calendar pour faire du design !

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -5,17 +5,10 @@ class Test extends CI_Controller {
 
     public function index()
     {
-        $this->load->helper('calendar');
-
-        $css = array("test");
-        $js = array("debug");
-        $title = "Premier essai !";
-        $data = array( getCalendar(9306, '2017-03-27', '2017-03-31') );
-
-        $var = array(   "css" => $css,
-                        "js" => $js,
-                        "title" => $title,
-                        "data" => $data);
+        $var = array(   "css" => array('test'),
+                        "js" => array(),
+                        "title" => 'Header design',
+                        "data" => array() );
 
         show("testv", $var);
 
