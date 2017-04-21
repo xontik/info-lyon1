3a8ffb4320eb8fd6a136dd62369fceb958a83de5
BORLET BENJAMIN p1502484 <benjamin.borlet@etu.univ-lyon1.fr>
2017-04-21 08:37:58 +0200

    Ajout corps des models + autoload + dbname

---
+++ application/models/control_model.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xontik
+ * Date: 21/04/2017
+ * Time: 07:47
+ */
+
+class Control_model extends CI_Model {
+
+    public function __construct(){
+
+    }
+    public function getControl($controlId){
+
+    }
+    public function getControlWithMarks($controlId){
+
+    }
+    public function addControl($coeff,$div,$type,$idDSPromo,$idProf,$group){
+        
+    }
+
+    public function editControl($controlId,$coeff,$div,$type,$idDSPromo,$idProf,$group){
+
+    }
+    public function deleteControl($controlid){
+
+    }
+}
---
+++ application/models/mark_model.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: xontik
+ * Date: 13/04/2017
+ * Time: 15:42
+ */
+class Mark_model extends CI_Model {
+
+
+    public function __construct()
+    {
+
+    }
+    public function getMarks($controlId){
+
+    }
+    public function addMark($controlId,$numStudent,$value){
+
+    }
+    public function addMarks($controlId,$a_numStudent,$a_value){
+
+    }
+    public function editMark($controlId,$numStudent,$newValue){
+
+    }
+    public function deleteMark($controlid,$numStudent){
+
+    }
+
+
+}
