65064c87975324d12ca90b30e4a899a14d85e0d0
DESKTOP-VSJNK9S\Enzo <enzo.baldisserri@outlook.fr>
2017-05-03 13:13:52 +0200

    Modif #4

--- assets/js/head.js
+++ assets/js/head.js
@@ -1,14 +1,29 @@
 $(function () {
 
+    // nav hover
+    // $('header > nav li').hover(
+    //     function() {
+    //         var params = {'height': '4em', 'line-height': '4em'};
+    //         $( this ).animate(params, 100);
+    //         $( this ).next('li').animate(params, 100);
+    //     },
+    //     function() {
+    //         var params = {'height': '3.5em', 'line-height': '3.5em'};
+    //         $( this ).animate(params, 100);
+    //         $( this ).next('li').animate(params, 100);
+    //     }
+    // );
+
+    // #header_profile
+    $('#header_profile').click( function() {
+        $( this ).children('ul').slideToggle(100);
+    });
+
     $( window ).resize(function()
     {
         var offset = ( $('#header_profile').width() - $('#header_profile > ul').width() ) / 2;
         $('#header_profile > ul').css( 'left', offset + 'px' );
     });
     $( window ).trigger('resize');
-
-    $('#header_profile').click( function() {
-       $( this ).children('ul').slideToggle(100);
-   });
 
 });
