219605ff47f0f9a4903937100e04d5b351b706f2
DESKTOP-VSJNK9S\Enzo <enzo.baldisserri@outlook.fr>
2017-08-17 19:34:59 +0200

    Suppression du footer vide
    Génant pour designer les pages,
    quitte à le remettre si on en utilise un finalement.

--- application/views/includes/foot.php
+++ application/views/includes/foot.php
@@ -1,6 +1,3 @@
-        <footer>
-            <p> Footer here</p>
-        </footer>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="<?php echo js_url('head'); ?>"></script>
