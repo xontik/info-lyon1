cc3164b4adb6c1795d741f92a773912d0d53c36e
Emma Glasson <ma.glasson@free.fr>
2017-09-30 16:39:03 +0200

    mise en forme du formulaire

--- application/views/Professeur/addMarks.php
+++ application/views/Professeur/addMarks.php
@@ -4,18 +4,19 @@
   <p><?php echo $data['matiere']->codeMatiere." - ".$data['matiere']->nomMatiere; ?> </p>
   <form method="post" action="<?php echo base_url("process_professeur/addmarks/" . ($data['control']->idControle)); ?>">
     <table>
-      <tr>
-        <th>Nom</th>
-        <th>Prenom</th>
-        <th>Note</th>
-      </tr>
-
+		<thead>
+		  <tr>
+			<th>Nom</th>
+			<th>Prenom</th>
+			<th>Note</th>
+		  </tr>
+		</thead>
       <?php
       foreach ($data['marks'] as $mark) {
         echo "<tr>";
         echo "<td>". $mark->nom. "</td>";
         echo "<td>". $mark->prenom. "</td>";
-        echo "<td><input type='text' name='".$mark->numEtudiant."' value='".((!is_null($mark->valeur))?$mark->valeur:"")."'/></td>";
+        echo "<td><input type='number' name='".$mark->numEtudiant."' value='".((!is_null($mark->valeur))?$mark->valeur:"")."'/></td>";
         echo "</tr>";
 
       }
