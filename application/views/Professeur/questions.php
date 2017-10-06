2761f820d93d8b046fce43568f5d2ebc617bdc7c
Kyle <espkyle@gmail.com>
2017-10-06 22:50:48 +0200

    Avancée 2 questions prof

--- application/views/Professeur/questions.php
+++ application/views/Professeur/questions.php
@@ -1,24 +1,33 @@
 <main>
 	<section>
 		<header>Les questions</header>
-		<?php
-		foreach ($data['profQuestions'] as $profQuestion) {
-			$student = $this->studentMod->getStudent($profQuestion->numEtudiant);?>
-			<div><?php echo $profQuestion->numEtudiant . ' - ' . $student->prenom . ' ' . strtoupper($student->nom);?></div>
-			<div><?php echo $profQuestion->texte; ?></div>
-		<?php
-			$listeReponses = $this->repMod->getAnswers($profQuestion->idQuestion);
-			foreach($listeReponses as $reponse){
-				echo '<p>'. $reponse->texte. '</p>';
-			} ?>
-			<form action="<?php echo current_url();?>" method="POST">
-				<input type="hidden" name="idQuestion" value ="<?php echo $profQuestion->idQuestion;?>"/>
-				<input type="text" name="texte" />
-				<input type="submit" value = "Répondre" />
-			</form>
+		<ul>
 			<?php
-		}
-		?>
+			foreach ($data['profQuestions'] as $profQuestion) {?>
+				<div>
+					<?php $student = $this->studentMod->getStudent($profQuestion->numEtudiant);?>
+					<li class = "question qr">
+						<?php echo $profQuestion->texte;?>
+						<span><?php echo $profQuestion->numEtudiant . ' - ' . $student->prenom . ' ' . strtoupper($student->nom);?></span>
+					</li>
+					<ul>
+						<?php
+						$listeReponses = $this->repMod->getAnswers($profQuestion->idQuestion);
+						foreach($listeReponses as $reponse){
+							echo '<li class="reponse qr">'. $reponse->texte. '</li>';
+						} 
+						?>
+						<form action="<?php echo current_url();?>" method="POST">
+							<input type="hidden" name="idQuestion" value ="<?php echo $profQuestion->idQuestion;?>"/>
+							<input type="text" name="texte" autocomplete = "off"/>
+							<input type="submit" value = "Répondre" />
+						</form>
+					</ul>
+				</div>
+			<?php
+			}
+			?>
+		</ul>
 	</section>
 </main>
 
