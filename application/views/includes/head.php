14bf101d4ed83da227c71673f6d892ac4028fde8
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2017-10-11 20:35:36 +0200

    Mise en place de Materialize

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -3,6 +3,12 @@
     <head>
         <title><?= isset($title) ? $title : 'Teckmeb' ?></title>
         <meta charset="utf-8">
+        <!-- Import Google Icon Font -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- Import materialize.css -->
+        <link type="text/css" rel="stylesheet" href="<?= css_url('materialize') ?>"  media="screen,projection">
+
+        <!-- Let browser know website is optimized for mobile -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php
         if ( !empty($css) ) {
