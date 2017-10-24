01f5f4b264fd45764148549c674808ee16824822
Kyle <espkyle@gmail.com>
2017-10-24 17:51:59 +0200

    Mise a jour vue etudiant absence

--- application/views/Etudiant/absences.php
+++ application/views/Etudiant/absences.php
@@ -1,23 +1,32 @@
-    <main>
-        <table id="tableauAbs" >
-            <thead>
-                <td>Date</td>
-                <td>Type</td>
-                <td>Justification</td>
-            </thead>
-            <?php
-            if(!empty($data['absences'])) {
-                foreach ($data['absences'] as $absence) { ?>
-                    <tr>
-                        <td><?php echo $absence->dateDebut; ?></td>
-                        <td><?php echo $absence->typeAbsence; ?></td>
-                        <td><?php echo $absence->justifiee ? 'Oui' : 'Non' ?></td>
-                    </tr>
-                <?php }
-            } else { ?>
-                <tr>
-                    <td colspan="99">Pas d'absences</td>
-                </tr>
-            <?php } ?>
-        </table>
-    </main>
+<main>
+    <table class="bordered">
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Horaire</th>
+                <th>Type</th>
+                <th>Justification</th>
+            </tr>
+        </thead>
+  		<tbody>
+          <?php
+          if(!empty($data['absences'])) {
+              foreach ($data['absences'] as $absence) { 
+					$dateD = new DateTime($absence->dateDebut);
+					$dateF = new DateTime($absence->dateFin);?>
+                  <tr <?= ($absence->justifiee == 0)?'class="red-text text-accent-4"':'' ?>>
+                    <td><?= $dateD->format('d/m/Y') ?></td>
+                    <td><?= $dateD->format('H:i') .' &rarr; '. $dateF->format('H:i') ?></td>
+                    <td><?= $absence->typeAbsence ?></td>
+                    <td><?= ($absence->justifiee == 0)?'Injustifiée':'Justifiée' ?></td>
+                  </tr>
+              <?php }
+          } else { ?>
+              <tr>
+                  <td colspan="99">Aucune absence</td>
+              </tr>
+          <?php } ?>
+  		</tbody>
+    </table>
+</main>
+   
