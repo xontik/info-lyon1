d6404843da57ceaa59d2fdb6a05c87575e5f57ec
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2017-10-24 09:28:44 +0200

    Optimisation de l'affichage de #side-edt-large

--- application/views/includes/side-edt.php
+++ application/views/includes/side-edt.php
@@ -43,90 +43,51 @@ ksort($timetable);
                         <div>Pas de cours</div>
                     <?php
                     } else {
-                        // TODO $timetable is sorted, insert fill in one time
                         $timeAtDate = $date->format('H:i');
-                        $events = array();
-                        $events_times = array();
 
-                        foreach ($timetable as $event) {
-                            $classes = array('hoverable', 'valign-wrapper');
-                            if (isset($timeAtDate) && $timeAtDate >= $event['time_start'] && $timeAtDate < $event['time_end']) {
-                                $classes[] = 'z-depth-2';
-                                unset($timeAtDate);
-                            }
-
-                            $event_dom = '<div class="' . join(' ', $classes) . '" '
-                                . 'style="height: '
-                                . computeTimeToHeight($event['time_start'], $event['time_end'])
-                                . '; ">' . PHP_EOL;
-                            $event_dom .= '<a href="' . $edt_url . '" class="black-text">' . PHP_EOL;
-
-                            $event_dom .= '<h5 title="' . $event['name'] .'" class="truncate">' . $event['name'] . '</h5>' . PHP_EOL;
-                            $event_dom .= '<div class="truncate">' . $event['teachers'] . '</div>' . PHP_EOL;
-                            $event_dom .= '<div>' . $event['groups'] . '</div>' . PHP_EOL;
-
-                            $event_dom .= '<div><i class="material-icons">location_on</i>'
-                                . $event['location']
-                                . '</div>' . PHP_EOL;
-
-                            $event_dom .= '</a>' . PHP_EOL;
-                            $event_dom .= '</div>' . PHP_EOL;
-
-                            $events[$event['time_start']] = $event_dom;
-                            $events_times[$event['time_start']] = $event['time_end'];
-                        }
-
-                        ksort($events, SORT_STRING);
-
-                        // If time is not during an event
-                        // Select next event
-                        if (isset($timeAtDate)) {
-                            // Reset internal array pointer
-                            $value = reset($events);
-
-                            while ($value !== FALSE) {
-                                $time = key($events);
-
-                                if ($time > $timeAtDate) {
-                                    // add class current-event
-                                    $value = substr_replace($value, 'current-event', 12, 0);
-                                    $items[$time] = $value;
-                                    break;
-                                }
-                                $value = next($events);
-                            }
+                        function fill_time($from, $to) {
+                            ?><div class="fill" style="height: <?= computeTimeToHeight($from, $to) ?>"></div>
+                            <?php
                         }
 
                         $lastTimeEnd = null;
 
-                        // Fill the time
-                        foreach ($events as $time => $event) {
+                        foreach ($timetable as $event) {
+                            // Fill time
                             if (is_null($lastTimeEnd)) {
                                 // Fill if day doesn't begin at 08:00
-                                if ($time !== '08:00') {
-                                    $events['08:00'] = '<div class="fill" '
-                                        . 'style="height: ' . computeTimeToHeight('08:00', $time) . ';">'
-                                        . '</div>' . PHP_EOL;
+                                if ($event['time_start'] !== '08:00') {
+                                    fill_time('08:00', $event['time_start']);
                                 }
-                            } else if ($lastTimeEnd !== $time) {
-                                $events[$lastTimeEnd] = '<div class="fill" '
-                                    . 'style="height: ' . computeTimeToHeight($lastTimeEnd, $time) . ';">'
-                                    . '</div>' . PHP_EOL;
+                            } else if ($lastTimeEnd !== $event['time_start']) {
+                                fill_time($lastTimeEnd, $event['time_start']);
                             }
-                            $lastTimeEnd = $events_times[$time];
+                            $lastTimeEnd = $event['time_end'];
+
+                            $classes = array('hoverable', 'valign-wrapper');
+                            if (isset($timeAtDate) && $timeAtDate < $event['time_end']) {
+                                $classes[] = 'z-depth-2';
+                                unset($timeAtDate);
+                            }
+
+                            ?>
+                                <div class="<?= join(' ', $classes) ?>"
+                                    style="height: <?= computeTimeToHeight($event['time_start'], $event['time_end']) ?>; ">
+                                    <a href="<?= $edt_url ?>" class="black-text">
+                                        <h5 title="<?= $event['name'] ?>" class="truncate"><?= $event['name'] ?></h5>
+                                        <div class="truncate"><?= $event['teachers'] ?></div>
+                                        <div><?= $event['groups'] ?></div>
+                                        <div><i class="material-icons">location_on</i><?= $event['location'] ?></div>
+                                    </a>
+                                </div>
+                                <?php
                         }
 
                         // Add a fill if day doesn't end at 18:00
                         if (!is_null($lastTimeEnd) && $lastTimeEnd !== '18:00') {
-                            $events[$lastTimeEnd] = '<div class="fill" '
-                                . 'style="height: ' . computeTimeToHeight($lastTimeEnd, '18:00') . ';">'
-                            . '</div>' . PHP_EOL;
+                            fill_time($lastTimeEnd, '18:00');
                         }
 
-                        ksort($events, SORT_STRING);
-                        foreach ($events as $event) {
-                            echo($event);
-                        }
                     } ?>
                 </div>
             </div>
