49682913637ed815d7a5bd3869e1a483fea09854
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2017-11-18 15:43:23 +0100

    Ajout d'une icône

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -9,13 +9,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <style>
+        h3 > span {
+            vertical-align: middle;
+        }
+
+        p {
+            margin: 0 0 1rem;
+            font-size: 1.6rem;
+        }
+    </style>
 </head>
 <body>
     <header>
         <nav><a href="/" class="brand-logo small-caps">Teckmeb</a></nav>
     </header>
     <main class="container section">
-        <h4>Erreur 404</h4>
+        <h3>
+            <i class="material-icons medium">error</i>
+            <span>Erreur 404</span>
+        </h3>
         <p>La page demandée n'existe pas</p>
         <a href="/" class="btn waves-effect waves-light">Revenir à l'acceuil</a>
     </main>
